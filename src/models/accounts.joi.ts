24f8b15dfebe8e7fdeaf0c62fc59a8c5a39eb1ea
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-07 02:58:25 -0500

    Fix account registration validation
    * Removing email form name field
    
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/models/accounts.joi.ts
+++ src/models/accounts.joi.ts
@@ -14,7 +14,7 @@ import { ACCOUNT_TYPE } from "@/models/accounts.enum";
  * Body schema validation for account register
  */
 export const AccountRegisterValidation = Joi.object().keys({
-  name: Joi.string().email().required(),
+  name: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().allow("").optional(),
   about: Joi.string().allow("").optional(),
