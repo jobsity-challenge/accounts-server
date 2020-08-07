dfb0c8f4beb5ec058a49eee60d4496e735d5140c
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-06 23:42:23 -0500

    Adding third party utilities
    * IKOA Business Opportunity Core Server (personal utility library)
    
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/vendor/ikoabo/models/errors.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * @Author: Reinier Millo Sánchez
+ * @Date:   2020-03-24T04:10:10-05:00
+ * @Email:  reinier.millo88@gmail.com
+ * @Project: IKOABO Core Microservice API
+ * @Filename: errors.ts
+ * @Last modified by:   Reinier Millo Sánchez
+ * @Last modified time: 2020-03-24T04:11:04-05:00
+ * @Copyright: Copyright 2020 IKOA Business Opportunity
+ */
+
+ export enum ERRORS {
+   INVALID_OPERATION = 701,
+   OBJECT_NOT_FOUND = 702,
+   OBJECT_DUPLICATED = 703,
+   INVALID_FIELDS = 704,
+ }
---
+++ src/vendor/ikoabo/models/status.enum.ts
@@ -0,0 +1,6 @@
+export enum BASE_STATUS {
+  BS_SOFT_DELETE = -1,
+  BS_UNKNOWN = 0,
+  BS_DISABLED = 1,
+  BS_ENABLED = 2,
+}
