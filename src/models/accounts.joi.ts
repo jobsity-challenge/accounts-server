020e88f6df001575b55dca8224d7492f55890c27
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-07 01:26:49 -0500

    Adding account JOI schema validation
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/models/accounts.joi.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2020 IKOA Business Opportunity
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <reinier.millo88@gmail.com>
+ *
+ * This file is part of the JobSity Challenge.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+import { Joi } from "@/vendor/ikoabo/middlewares/validator.middleware";
+import { ACCOUNT_TYPE } from "@/models/accounts.enum";
+
+/**
+ * Body schema validation for account register
+ */
+export const AccountRegisterValidation = Joi.object().keys({
+  name: Joi.string().email().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().allow("").optional(),
+  about: Joi.string().allow("").optional(),
+  type: Joi.number().min(ACCOUNT_TYPE.AT_UNKNOWN).max(ACCOUNT_TYPE.AT_BOT).required(),
+});
+
+/**
+ * Body schema validation for account login
+ */
+export const AccountLoginValidation = Joi.object().keys({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
