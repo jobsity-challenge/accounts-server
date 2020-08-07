93959f24921e811d4aded5622e28e05eb9d7eec5
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-07 01:27:07 -0500

    Adding service errors definition
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/models/errors.enum.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2020 IKOA Business Opportunity
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <reinier.millo88@gmail.com>
+ *
+ * This file is part of the JobSity Challenge.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+
+/**
+ * Predefined account errors
+ */
+export enum ACCOUNT_ERROR {
+  ERR_UNKNOWN = 500,
+  ERR_INVALID_ACCESS_TOKEN = 1000,
+  ERR_INVALID_CREDENTIALS = 1001,
+  ERR_INVALID_ROLE = 1002,
+}
