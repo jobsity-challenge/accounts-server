8d5998137c3657ff62576860fdcf68095471de3d
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-08-06 23:43:16 -0500

    Adding initial service cluster application
    * Adding base service settings
    * Initialize server cluster
    
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2020 IKOA Business Opportunity
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <reinier.millo88@gmail.com>
+ *
+ * This file is part of the JobSity Challenge.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+import "module-alias/register";
+import { ClusterServer } from "@/vendor/ikoabo/controllers/cluster.controller";
+import { ServiceSettings } from "@/settings/service.settings";
+
+/* Initialize cluster server */
+const clusterServer = ClusterServer.setup(ServiceSettings);
+
+/* Run cluster with base routes */
+clusterServer.run({
+
+});
